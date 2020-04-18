2502f947581e92de82ad2a425e961ed80ee1bb32
Dan Bock <danbock@gmail.com>
2020-04-18 04:53:52 -0400

    Add jest unit tests

---
+++ src/functions.test.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { getStateData } from './functions'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const mockResponse = {
+  data:
+    'date,state,fips,cases,deaths\n' +
+    '2020-01-21,Washington,53,1,0\n' +
+    '2020-01-22,Washington,53,1,0\n' +
+    '2020-01-23,Washington,53,1,0\n' +
+    '2020-01-24,Illinois,17,1,0\n' +
+    '2020-01-24,Washington,53,1,0\n' +
+    '2020-01-25,California,06,1,0\n',
+}
+
+it('gets the data', async () => {
+  mockedAxios.get.mockResolvedValue(mockResponse)
+  const data = await getStateData('Washington')
+  expect(data).toHaveLength(4)
+})

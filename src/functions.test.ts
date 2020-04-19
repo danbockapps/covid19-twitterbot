247f77687b957345ccf96a186fa1be24a5ea098d
Dan Bock <danbock@gmail.com>
2020-04-19 16:48:37 -0400

    Generate tweet text

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,5 +1,11 @@
+import { ChronoUnit, LocalDate } from '@js-joda/core'
 import axios from 'axios'
-import { getStateData } from './functions'
+import {
+  getDayMetric,
+  getStateData,
+  getStateTweetText,
+  StateDay,
+} from './functions'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -15,10 +21,35 @@ const mockResponse = {
     '2020-01-25,California,06,1,0\n',
 }
 
-it('gets the data', async () => {
+let washData: StateDay[]
+
+beforeAll(async () => {
   mockedAxios.get.mockResolvedValue(mockResponse)
-  const data = await getStateData('Washington')
-  expect(data).toHaveLength(4)
-  expect(data[3].date).toEqual(new Date('2020-01-24'))
-  expect(data[3].cases).toEqual(1)
+  washData = await getStateData('Washington')
+})
+
+it('gets the data', async () => {
+  expect(washData).toHaveLength(4)
+  expect(washData[3].date).toEqual(LocalDate.parse('2020-01-24'))
+  expect(washData[3].cases).toEqual(1)
+})
+
+it('gets metrics from the data', () => {
+  const daysSince124 = LocalDate.parse('2020-01-24').until(
+    LocalDate.now(),
+    ChronoUnit.DAYS,
+  )
+
+  const daysSince125 = LocalDate.parse('2020-01-25').until(
+    LocalDate.now(),
+    ChronoUnit.DAYS,
+  )
+
+  expect(getDayMetric(washData, 'cases', daysSince124)).toEqual(1)
+  expect(getDayMetric(washData, 'cases', daysSince125)).toEqual(0)
+})
+
+it('constructs state tweet text', async () => {
+  const tweetText = await getStateTweetText('Washington')
+  expect(tweetText).toEqual(expect.stringContaining('30 days ago'))
 })
--- src/functions.ts
+++ src/functions.ts
@@ -1,10 +1,11 @@
+import { LocalDate } from '@js-joda/core'
 import axios from 'axios'
 import Papa from 'papaparse'
 
 const getData = () =>
   axios
     .get<string>(
-      'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
+      'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv',
     )
     .then(response => Papa.parse(response.data, { header: true }).data)
 
@@ -14,11 +15,40 @@ export const getStateData: (state: string) => Promise<StateDay[]> = state =>
       .filter((row: RawStateDay) => row.state === state)
       .map((row: RawStateDay) => ({
         ...row,
-        date: new Date(row.date),
+        date: LocalDate.parse(row.date),
         cases: Number(row.cases),
         deaths: Number(row.deaths),
-      }))
+      })),
   )
+
+export const getStateTweetText = async (state: string) => {
+  const data = await getStateData(state)
+  return `Updated data for ${state}
+
+Cases
+${getDayMetric(data, 'cases', 1)}: Now
+${getDayMetric(data, 'cases', 2)}: Yesterday
+${getDayMetric(data, 'cases', 8)}: 7 days ago
+${getDayMetric(data, 'cases', 31)}: 30 days ago
+
+Deaths
+${getDayMetric(data, 'deaths', 1)}: Now
+${getDayMetric(data, 'deaths', 2)}: Yesterday
+${getDayMetric(data, 'deaths', 8)}: 7 days ago
+${getDayMetric(data, 'deaths', 31)}: 30 days ago
+`
+}
+
+export const getDayMetric: (
+  data: StateDay[],
+  metric: 'cases' | 'deaths',
+  daysSince: number,
+) => number = (data, metric, daysSince) => {
+  const row: StateDay | undefined = data.find(element =>
+    element.date.equals(LocalDate.now().minusDays(daysSince)),
+  )
+  return row ? row[metric] : 0
+}
 
 interface RawStateDay {
   date: string
@@ -29,7 +59,7 @@ interface RawStateDay {
 }
 
 export interface StateDay {
-  date: Date
+  date: LocalDate
   state: string
   fips: string
   cases: number

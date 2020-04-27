7d3f702b9f5a19ed5541dedc39547ae9748ee2d4
Dan Bock <danbock@gmail.com>
2020-04-26 22:38:03 -0400

    Add DynamoDB

---
+++ src/dynamodb.ts
@@ -0,0 +1,22 @@
+import AWS from 'aws-sdk'
+
+AWS.config.update({
+  region: 'us-east-2',
+})
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+export const dateExistsInDb = async (date: String) => {
+  const result = await docClient
+    .get({ TableName: 'Tweets', Key: { date } })
+    .promise()
+
+  if (result.Item) return true
+  else return false
+}
+
+export const insertDateIntoDb = async (date: String) => {
+  return await docClient
+    .put({ TableName: 'Tweets', Item: { date } })
+    .promise()
+}
